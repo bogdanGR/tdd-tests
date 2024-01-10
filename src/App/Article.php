500dac285709e84b8e7c02da0cb4522a64cfc981
bogdanGR <fant1k450@gmail.com>
2024-01-10 07:12:38 +0200

    test slug

--- src/App/Article.php
+++ src/App/Article.php
@@ -5,4 +5,14 @@ namespace App;
 class Article
 {
     public $title;
+
+    public function getSlug()
+    {
+        $slug = $this->title;
+
+        $slug = preg_replace('/\s+/', '_', $slug);
+        $slug = preg_replace('/[^\w]+/', '', $slug);
+
+        return trim($slug, '_');
+    }
 }
--- tests/ArticleTest.php
+++ tests/ArticleTest.php
@@ -3,10 +3,46 @@
 use PHPUnit\Framework\TestCase;
 class ArticleTest extends TestCase
 {
+    protected $article;
+
+    public function setUp(): void
+    {
+        $this->article = new App\Article();
+    }
     public function testTitleIsEmptyByDefault()
     {
-        $article = new App\Article();
 
-        $this->assertEmpty($article->title);
+        $this->assertEmpty($this->article->title);
+    }
+
+    public function testSlugIsEmptyWithNoTitle()
+    {
+        $this->assertSame($this->article->getSlug(), "");
+    }
+
+    public function testSlugHasSpaceReplacedByUnderscores()
+    {
+        $this->article->title = 'An example article';
+
+        $this->assertEquals($this->article->getSlug(), 'An_example_article');
+    }
+
+    public function testSlugHasWhitespacesReplacedBySingleUnderscore()
+    {
+        $this->article->title = "An example \n article";
+
+        $this->assertEquals($this->article->getSlug(), 'An_example_article');
+    }
+
+    public function testSlugDoesNotStartOrEndWithAnUnderscore()
+    {
+        $this->article->title = " An example article ";
+        $this->assertEquals($this->article->getSlug(), 'An_example_article');
+    }
+
+    public function testSlugDoesNotHaveAnyNonwordCharacters()
+    {
+        $this->article->title = "Read! This! Now!";
+        $this->assertEquals($this->article->getSlug(), 'Read_This_Now');
     }
 }
